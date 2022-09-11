d8ef3f518fc2983f415270b1f318bf2e96a5bc8f
ayemin6796 <diamalt541@gmail.com>
2022-09-11 22:29:28 +0630

    Second Try

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import './style.scss';
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
+let head = document.querySelector(".more-head");
+
+head.addEventListener("click",function(e){
+    console.dir(e.target)
+    // e.target.parentNode.classList.remove("show");
+})
+
+const dropdownElementList = document.querySelectorAll('.dropdown-self')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
+
+dropdownList.map(function(e){
+    console.dir(e)
+})
